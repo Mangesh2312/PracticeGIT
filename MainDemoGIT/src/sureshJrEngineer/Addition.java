c5bd7ac535d38bca5145b23ce687864f4ebfa0ed
Mangesh <mangeshrane24@gmail.com>
2026-02-28 23:36:15 +0530

    changes by suresh jr engineer

---
+++ MainDemoGIT/src/sureshJrEngineer/Addition.java
@@ -0,0 +1,16 @@
+package sureshJrEngineer;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		System.out.println("I am Addition Class");
+	}
+	
+	public void addTwoNumbers() {
+		int a = 10;
+		int b = 20;
+		int sum = a + b;
+		System.out.println("The sum of two numbers is: " + sum);
+	}
+	
+}
