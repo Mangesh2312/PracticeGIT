f011d73f05b39692d7acffa43ba9334a1da06796
Mangesh <mangeshrane24@gmail.com>
2026-03-01 17:50:42 +0530

    main engineer Mangesh changes added

---
+++ MainDemoGIT/src/mangeshMainEngineer/TestCaseB.java
@@ -0,0 +1,15 @@
+package mangeshMainEngineer;
+
+public class TestCaseB {
+	public static void main(String[] args) {
+		System.out.println("I am TestCaseB Class");
+		testCaseBMethod();
+	}
+
+	public static void testCaseBMethod() {
+		System.out.println("This is a method in TestCaseB class");
+	}
+	
+	
+	
+}
