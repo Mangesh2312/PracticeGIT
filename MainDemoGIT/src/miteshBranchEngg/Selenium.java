ce5eea2f16960270009efed94e68821746f2aee1
Mangesh <mangeshrane24@gmail.com>
2026-03-02 19:33:32 +0530

    Mitesh Branch Engg

---
+++ MainDemoGIT/src/miteshBranchEngg/Selenium.java
@@ -0,0 +1,17 @@
+package miteshBranchEngg;
+
+public class Selenium {
+	
+	public void openBrowser() {
+		System.out.println("Opening the browser");
+	}
+	
+	public void goToURL() {
+		System.out.println("Navigating to the URL");
+	}
+	
+	public void loginToApplication() {
+		System.out.println("Logging into the application");
+	}
+
+}
