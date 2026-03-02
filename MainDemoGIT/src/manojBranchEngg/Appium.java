68a21e79902e14a05b6bb6b6c0435f094392e355
Mangesh <mangeshrane24@gmail.com>
2026-03-02 20:00:07 +0530

    manoj branch engg

---
+++ MainDemoGIT/src/manojBranchEngg/Appium.java
@@ -0,0 +1,20 @@
+package manojBranchEngg;
+
+public class Appium {
+	
+	public void setUPMobile()
+	{
+		System.out.println("Set up mobile");
+	}
+	
+	public void downloadApp()
+	{
+		System.out.println("Download app");
+	}
+	
+	public void connectToTheApp()
+	{
+		System.out.println("Connect to the app");
+	}
+
+}
