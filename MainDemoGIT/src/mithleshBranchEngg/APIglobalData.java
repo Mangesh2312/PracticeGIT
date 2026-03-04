fb895fd46f57f5b9e9a3c4ddd70a724c413e0542
Mangesh <mangeshrane24@gmail.com>
2026-03-04 20:46:23 +0530

    mithilesh branch engineer

---
+++ MainDemoGIT/src/mithleshBranchEngg/APIglobalData.java
@@ -0,0 +1,10 @@
+package mithleshBranchEngg;
+
+public class APIglobalData {
+	
+	public void globalData()
+	{
+		System.out.println("Global data is in progress");
+	}
+
+}
---
+++ MainDemoGIT/src/mithleshBranchEngg/APItesting.java
@@ -0,0 +1,15 @@
+package mithleshBranchEngg;
+
+public class APItesting {
+	
+	public void testAPI()
+	{
+		System.out.println("API testing is in progress");
+	}
+	
+	public void apiSetUp()
+	{
+		System.out.println("API set up is in progress");
+	}
+
+}
